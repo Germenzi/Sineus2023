8f1ffc1d1cb2b77a0c36b9a2c1917c6fe36bb581
Nikita\Nick <kineeetik@gmail.com>
2024-10-04 23:36:34 +0300

    Улучшил изменения

--- Namespaces/ScenesNamespace.gd
+++ Namespaces/ScenesNamespace.gd
@@ -5,3 +5,4 @@ class_name ScenesNamespace
 const MAIN_MENU_SCENE_FILEPATH : String = "res://main.tscn"
 const PLAYING_SCENE_FILEPATH : String = "res://GameScenes/playing_scene.tscn"
 const RUNE_MATCH_SCENE_FILEPATH : String = "res://GameScenes/GameplayScenes/rune_match.tscn"
+const EXTENDING_CIRCLE_SCENE_FILEPATH : String = "res://UIScenes/extending_circle.tscn"

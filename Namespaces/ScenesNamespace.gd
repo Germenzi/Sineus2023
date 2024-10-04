0c4318d40f532be61d3719ca03a0a1b14e2b8c33
Nikita\Nick <kineeetik@gmail.com>
2024-10-04 21:02:37 +0300

    Сделал сцену с несколькими рунами

--- Namespaces/ScenesNamespace.gd
+++ Namespaces/ScenesNamespace.gd
@@ -4,3 +4,4 @@ class_name ScenesNamespace
 
 const MAIN_MENU_SCENE_FILEPATH : String = "res://main.tscn"
 const PLAYING_SCENE_FILEPATH : String = "res://GameScenes/playing_scene.tscn"
+const RUNE_MATCH_SCENE_FILEPATH : String = "res://GameScenes/GameplayScenes/rune_match.tscn"

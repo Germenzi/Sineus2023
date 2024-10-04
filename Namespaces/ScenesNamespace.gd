638e7dcad7c3b3e39750e164cb5b695c516ad46a
Nikita\Nick <kineeetik@gmail.com>
2024-10-04 19:49:49 +0300

    чето наделал тут

---
+++ Namespaces/ScenesNamespace.gd
@@ -0,0 +1,6 @@
+extends Object
+
+class_name ScenesNamespace
+
+const MAIN_MENU_SCENE_FILEPATH : String = "res://main.tscn"
+const PLAYING_SCENE_FILEPATH : String = "res://GameScenes/playing_scene.tscn"

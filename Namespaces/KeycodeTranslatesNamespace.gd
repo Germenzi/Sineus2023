3a58cdc32df53c8367455e39754393427759c7af
Nikita\Nick <kineeetik@gmail.com>
2024-10-05 14:00:13 +0300

    Сделал возможность вбивать слова на обеих раскладках клавиатуры

---
+++ Namespaces/KeycodeTranslatesNamespace.gd
@@ -0,0 +1,22 @@
+extends Object
+
+class_name KeycodeTranslatorNamespace
+
+# Словарь с кейкодами и объединенными символами для английской и русской клавиатуры
+const KEYCODE_MAP : Dictionary = {
+	KEY_A: "AФ", KEY_B: "BИ", KEY_C: "CС", KEY_D: "DВ", KEY_E: "EУ",
+	KEY_F: "FА", KEY_G: "GП", KEY_H: "HР", KEY_I: "IШ", KEY_J: "JО",
+	KEY_K: "KЛ", KEY_L: "LД", KEY_M: "MЬ", KEY_N: "NТ", KEY_O: "OЩ",
+	KEY_P: "PЗ", KEY_Q: "QЙ", KEY_R: "RК", KEY_S: "SЫ", KEY_T: "TЕ",
+	KEY_U: "UГ", KEY_V: "VМ", KEY_W: "WЦ", KEY_X: "XЧ", KEY_Y: "YН",
+	KEY_Z: "ZЯ", KEY_0: "0", KEY_1: "1", KEY_2: "2", KEY_3: "3",
+	KEY_4: "4", KEY_5: "5", KEY_6: "6", KEY_7: "7", KEY_8: "8",
+	KEY_9: "9",
+	
+	# Символы точки и запятой
+	KEY_PERIOD: ".Ю", KEY_COMMA: ",Б",
+	
+	# Специальные символы
+	KEY_SEMICOLON: ";Ж", KEY_APOSTROPHE: "'Э", KEY_BRACKETLEFT: "[Х",
+	KEY_BRACKETRIGHT: "]Ъ"
+}

d5454b8440cabc26e8b3815d8c4cca6f3bd4c677
Nikita\Nick <kineeetik@gmail.com>
2024-10-05 11:58:49 +0300

    Добавил позу молитвы

---
+++ Namespaces/TexturesNamespace.gd
@@ -0,0 +1,6 @@
+extends Object
+
+class_name TexturesNamespace
+
+const SHAMAN_PRAY_POSE_TEXTURE : Texture2D = preload("res://assets/images/prayer_pose.png")
+const SHAMAN_DANCING_TEXTURE_FRAME : TextureFrame = preload("res://assets/images/dancing_texture_frames.tres")

be142cd569b8bfbf58c95b1b15192a107fa046d7
Nikita\Nick <kineeetik@gmail.com>
2024-10-05 18:28:39 +0300

    Добавил яйцу стадии

--- Namespaces/TexturesNamespace.gd
+++ Namespaces/TexturesNamespace.gd
@@ -6,3 +6,7 @@ const SHAMAN_PRAY_POSE_TEXTURE : Texture2D = preload("res://assets/images/prayer
 const SHAMAN_DANCING_TEXTURE_FRAME : TextureFrame = preload("res://assets/images/dancing_texture_frames.tres")
 const SHAMAN_LOSE_POSE_TEXTURE : Texture2D = preload("res://assets/images/loser_pose.png")
 const SHAMAN_WIN_POSE_TEXTURE : Texture2D = preload("res://assets/images/victory_pose.png")
+
+const EGG_STATE1_TEXTURE : Texture2D = preload("res://assets/images/egg_1.png")
+const EGG_STATE2_TEXTURE : Texture2D = preload("res://assets/images/egg_2.png")
+const EGG_STATE3_TEXTURE : Texture2D = preload("res://assets/images/egg_3.png")

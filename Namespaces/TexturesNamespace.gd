d6996385e24ff0e9917d1390a24bd379309f1dea
Nikita\Nick <kineeetik@gmail.com>
2024-10-05 13:14:32 +0300

    Добавил позы победы и поражения

--- Namespaces/TexturesNamespace.gd
+++ Namespaces/TexturesNamespace.gd
@@ -4,3 +4,5 @@ class_name TexturesNamespace
 
 const SHAMAN_PRAY_POSE_TEXTURE : Texture2D = preload("res://assets/images/prayer_pose.png")
 const SHAMAN_DANCING_TEXTURE_FRAME : TextureFrame = preload("res://assets/images/dancing_texture_frames.tres")
+const SHAMAN_LOSE_POSE_TEXTURE : Texture2D = preload("res://assets/images/loser_pose.png")
+const SHAMAN_WIN_POSE_TEXTURE : Texture2D = preload("res://assets/images/victory_pose.png")
